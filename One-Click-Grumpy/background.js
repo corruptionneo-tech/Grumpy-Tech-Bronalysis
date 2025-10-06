a44d9f08c178ade8cf0a3b307ab5c9989c684d8d
corruptionneo-tech <corruptionneo@gmail.com>
2025-10-05 22:02:04 -0400

    Add files via upload
    Changed to grok.com for analysis... will add config option tomorrow

--- One-Click-Grumpy/background.js
+++ One-Click-Grumpy/background.js
@@ -1,6 +1,6 @@
 chrome.action.onClicked.addListener((tab) => {
   const currentUrlEncoded = encodeURIComponent(tab.url);
-  const baseUrl = "https://x.com/i/grok?text=use+browse_page+on+URL+https%3A%2F%2Fx.com%2Ftwinforces%2Fstatus%2F1974839758271783080+apply+to+content+from+browse_page+tool+on+URL+";
+  const baseUrl = "https://grok.com/?q=use+browse_page+on+URL+https%3A%2F%2Fx.com%2Ftwinforces%2Fstatus%2F1974839758271783080+apply+to+content+from+browse_page+tool+on+URL+";
   const fullUrl = baseUrl + currentUrlEncoded;
   chrome.tabs.create({ url: fullUrl });
 });
